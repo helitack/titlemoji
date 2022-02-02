8bab750fa31068cea85818e7c5e9c9a7c2cfca35
Crispin Hoggarth <crispinhoggarth@gmail.com>
2022-02-01 23:59:28 -0700

    add emojis

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
 > Ensure you have `ncc` installed: `npm i -g @vercel/ncc`
 
 1. Compile `ncc build index.js --license licenses.txt`
-1. Commit `git add --all` `git commit -m ""`
+1. Commit `git add --all && git commit -m ""`
 1. Tag `git tag -a -m "" v1.1`
 1. Push `git push --follow-tags`
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const conventionalCommitsConfig = require('conventional-changelog-conventionalcommits');
+const parser = require('conventional-commits-parser').sync;
 
 
 async function run() {
   try {
-    console.log(`GITHUB_TOKEN ${process.env.GITHUB_TOKEN}`);
-    
     const client = github.getOctokit(process.env.GITHUB_TOKEN);
     
-    console.log(`client ${client}`);
-
     const contextPullRequest = github.context.payload.pull_request;
     if (!contextPullRequest) {
       throw new Error(
@@ -17,14 +15,8 @@ async function run() {
       );
     }
 
-    console.log(`contextPullRequest ${contextPullRequest}`);
-    
     const owner = contextPullRequest.base.user.login;
     const repo = contextPullRequest.base.repo.name;
-
-    console.log(`owner ${owner}`);
-    console.log(`repo ${repo}`);
-    console.log(`number ${contextPullRequest.number}`);
 
     const {data: pullRequest} = await client.pulls.get({
       owner,
@@ -33,9 +25,51 @@ async function run() {
     });
 
     const title = pullRequest.title;
-
     core.info(`Pull Request title: "${title}"`);
-    console.log(`Title Console Log: ${title}`);
+
+    const { parserOpts } = await conventionalCommitsConfig();
+    const parserResult = parser(title, parserOpts);
+
+    if (!parserResult.type) {
+      core.info(`No conventional commits type found in title: "${title}"`);
+      return;
+    }
+
+    let emoji = '';
+    switch(parserResult.type) {
+      case 'feat':
+        emoji = ':sparkles:';
+        break;
+      case 'fix':
+        emoji = ':bug:';
+        break;
+      case 'docs':
+        emoji = ':books:';
+        break;
+      case 'style':
+        emoji = ':art:';
+        break;
+      case 'refactor':
+        emoji = ':recycle:';
+        break;
+      case 'perf':
+        emoji = ':zap:';
+        break;
+      case 'test':
+        emoji = ':fire:';
+        break;
+      case 'chore':
+        emoji = ':construction:';
+        break;
+      case 'revert':
+        emoji = ':rewind:';
+        break;
+      default:
+        emoji = ':question:';
+    }
+
+    core.info(`Conventional commits type: "${parserResult.type}"`);
+    core.info(`Setting emoji to: "${emoji}"`);
     
   } catch (error) {
     core.setFailed(error.message);
